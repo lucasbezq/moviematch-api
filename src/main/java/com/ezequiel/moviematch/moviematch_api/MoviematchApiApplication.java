3a72d8099eba8eff38d428d68df4bfcf27264000
lucasbezq <lucasbarrosez@gmail.com>
2024-11-07 22:30:56 -0300

    feat: projeto inicializado

---
+++ src/main/java/com/ezequiel/moviematch/moviematch_api/MoviematchApiApplication.java
@@ -0,0 +1,13 @@
+package com.ezequiel.moviematch.moviematch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviematchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviematchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezequiel/moviematch/moviematch_api/MoviematchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezequiel.moviematch.moviematch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviematchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
