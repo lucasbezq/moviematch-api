078c9fc6f87aa5402f4889c1f86ddc712258034d
lucasbezq <lucasbarrosez@gmail.com>
2024-11-08 20:18:37 -0300

    feat: implementar records de gênero

---
+++ src/main/java/com/ezequiel/moviematch/api/controller/GenreController.java
@@ -0,0 +1,29 @@
+package com.ezequiel.moviematch.api.controller;
+
+import com.ezequiel.moviematch.api.converter.GenreRecordConverter;
+import com.ezequiel.moviematch.api.record.genre.GenreRecord;
+import com.ezequiel.moviematch.domain.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/{genre}")
+public class GenreController {
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private GenreRecordConverter genreRecordConverter;
+
+    @GetMapping
+    public List<GenreRecord> findAll() {
+        var genres = genreRepository.findAll();
+        return genreRecordConverter.toCollectionRecord(genres);
+    }
+
+}
